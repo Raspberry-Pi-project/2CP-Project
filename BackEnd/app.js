84721bce6fdf0c5cd7028caffafa8773e65dead6
Meridja Abdelouahab <na_meridja@esi.dz>
2025-02-17 12:00:39 +0100

    test backend

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,1 +1,13 @@
-console.log("testt")
+const express = require('express');
+const app = express();
+const port = 3100;
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+//console.log("path",path.join(__dirname, 'public'))
+app.get('/', (req, res) => {
+    res.send('Helloo World!');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
