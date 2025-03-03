979e494a20fb23b850c2983a5d50697ff2eebf32
Meridja Abdelouahab <154991307+AbdouMero19@users.noreply.github.com>
2025-03-03 12:42:54 +0100

    Add files via upload

---
+++ quizzey (1).sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 03 mars 2025 à 12:40
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `quizzey`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admins`
+--
+-- Erreur de lecture de structure pour la table quizzey.admins : #1932 - Table &#039;quizzey.admins&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.admins : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`admins`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `answers`
+--
+-- Erreur de lecture de structure pour la table quizzey.answers : #1932 - Table &#039;quizzey.answers&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.answers : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`answers`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `attempts`
+--
+-- Erreur de lecture de structure pour la table quizzey.attempts : #1932 - Table &#039;quizzey.attempts&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.attempts : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`attempts`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groupe`
+--
+-- Erreur de lecture de structure pour la table quizzey.groupe : #1932 - Table &#039;quizzey.groupe&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.groupe : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`groupe`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `priviliges`
+--
+-- Erreur de lecture de structure pour la table quizzey.priviliges : #1932 - Table &#039;quizzey.priviliges&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.priviliges : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`priviliges`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `published_quizzes`
+--
+-- Erreur de lecture de structure pour la table quizzey.published_quizzes : #1932 - Table &#039;quizzey.published_quizzes&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.published_quizzes : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`published_quizzes`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `questions`
+--
+-- Erreur de lecture de structure pour la table quizzey.questions : #1932 - Table &#039;quizzey.questions&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.questions : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`questions`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `quizzes`
+--
+-- Erreur de lecture de structure pour la table quizzey.quizzes : #1932 - Table &#039;quizzey.quizzes&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.quizzes : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`quizzes`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `students`
+--
+-- Erreur de lecture de structure pour la table quizzey.students : #1932 - Table &#039;quizzey.students&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.students : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`students`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `student_answers`
+--
+-- Erreur de lecture de structure pour la table quizzey.student_answers : #1932 - Table &#039;quizzey.student_answers&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.student_answers : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`student_answers`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `teachers`
+--
+-- Erreur de lecture de structure pour la table quizzey.teachers : #1932 - Table &#039;quizzey.teachers&#039; doesn&#039;t exist in engine
+-- Erreur de lecture des données pour la table quizzey.teachers : #1064 - Erreur de syntaxe près de &#039;FROM `quizzey`.`teachers`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
